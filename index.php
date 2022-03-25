97f41422f7d6d69a85a7357e28e5b70d0cdcb5b2
Mendrika-jolivet <mendrika.jolivet@yahoo.com>
2022-03-25 12:03:07 +0100

    post

--- index.php
+++ index.php
@@ -8,13 +8,13 @@ include('init.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercice</title>
+    <title>Home page</title>
 </head>
 <body>
     <?php
         if(isset($_SESSION['membre'])) {
     ?>
-        <h1>Bonjour <?php echo $_SESSION['membre']['pseudo'].?> !</h1>
+        <h1>Bonjour <?php echo $_SESSION['membre']['pseudo']?> !</h1>
     <?php
         } else {
     ?>
@@ -24,8 +24,18 @@ include('init.php');
     -
 
     <a href="connexion.php">connexion</a>
+    <br>
     <?php
        }
+    ?>
+    <?php
+    //affiche les event stockés dans la table post
+        $r = $pdo ->query('SELECT * FROM post');
+        while ($event = $r-> fetch(PDO::FETCH_ASSOC)) {
+            echo $event['titre'] . ' ' . $event['date_post'] . '<br>' . $event['content_post'] . '<br>';
+        }
+
+
     ?>
 </body>
 </html>
